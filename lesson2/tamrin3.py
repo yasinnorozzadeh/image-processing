4a7bfe554bae04c9ba7bc2095df984ef890d0924
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-07-18 01:37:40 +0430

    Update tamrin3.py

--- lesson2/tamrin3.py
+++ lesson2/tamrin3.py
@@ -1,17 +1,19 @@
 import cv2
 img1 = cv2.imread("Assignment22/hw2/board - test.bmp", 0)
 img2 = cv2.imread("Assignment22/hw2/board - origin.bmp", 0)
-img_1 = cv2.resize(img1, (700, 700))
-img_2 = cv2.resize(img2, (700, 700))
+img_1 = cv2.resize(img1, (800, 800))
+img_2 = cv2.resize(img2, (800, 800))
 img__2 = cv2.flip(img_2, 1)
-sub_imgs = cv2.subtract(img_1, img__2)
+sub_imgs = cv2.subtract(img__2, img_1)
+result1 = sub_imgs
+cv2.imshow("output1", result1)
 result = sub_imgs * 255
 h,w= result.shape
 for i in range(h):
     for j in range(w):
-        if result[i][j]< 0 :
-            result[i][j] =0
-        elif result[i][j]>255 :
-            result[i][j]=255
+        if result[i][j] < 0:
+            result[i][j] = 0
+        elif result[i][j] > 255:
+            result[i][j] = 255
 cv2.imshow("Out_Put", result)
 cv2.waitKey()
