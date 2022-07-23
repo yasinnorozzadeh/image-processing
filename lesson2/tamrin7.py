be6c51c5cc3bbe5c330aab1d8fb392ba9e8745d1
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-07-23 18:22:33 +0430

    Update tamrin7.py

--- lesson2/tamrin7.py
+++ lesson2/tamrin7.py
@@ -1,4 +1,3 @@
-
 import cv2
 import random
 import numpy as np
@@ -8,7 +7,7 @@ h ,w =img.shape
 for i in range(h):
     for j in range(w):
         r = random.random()
-        if r < 0.015:
-            img[i][j] = 0
+        if r < 0.06:
+            img[i][j] = random.randint(100,255)
 cv2.imshow("Out_Put" , img)
 cv2.waitKey()
